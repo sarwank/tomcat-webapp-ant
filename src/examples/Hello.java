6902234edac464a155a62bc2656eb77cca5a8423
Sarwan Kr. Sharma <sarwan.sharma@hotmail.com>
2022-01-08 00:36:27 +0530

    Update Hello.java

--- src/examples/Hello.java
+++ src/examples/Hello.java
@@ -49,7 +49,7 @@ public final class Hello extends HttpServlet {
         writer.println("</table>");
 
         writer.println("This is the double modified output of a servlet that is part of");
-        writer.println("the Hello, World application.");
+        writer.println("the Hello, RACKSPACE.");
 
         writer.println("</body>");
         writer.println("</html>");
